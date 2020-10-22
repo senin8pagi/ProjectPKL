c1a1971a473d0fba2181a870da1dce6005c1e82a
aweng-02 <72443451+aweng-02@users.noreply.github.com>
2020-10-22 22:11:47 +0700

    Update laporan_printmanifest.php

--- admin/laporan_printmanifest.php
+++ admin/laporan_printmanifest.php
@@ -1,83 +1,83 @@
  <!DOCTYPE html>
  <html>
  <head>
- 	<meta charset="utf-8">
- 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
- 	<title>Laporan Manifest</title>
- 	<link rel="stylesheet" href="../assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Laporan Manifest</title>
+  <link rel="stylesheet" href="../assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
  </head>
  <body>
 
- 	<center>
- 		<h4>LAPORAN MANIFEST</h4>
- 		<h4>SISTEM INFORMASI MLOKET(ORANGER)</h4>
- 	</center>
+  <center>
+   <h4>LAPORAN MANIFEST</h4>
+   <h4>SISTEM INFORMASI MLOKET(ORANGER)</h4>
+  </center>
 
 
- 	<?php 
-  if(isset($_GET['transaksi_tanggal']) && isset($_GET['kprk']) && isset($_GET['regional'])){
-  $tgl_trx = $_GET['transaksi_tanggal'];
-  $kprk       = $_GET['kprk'];
-  $regional   = $_GET['regional'];
-  ?>
+  <?php 
+  if (isset($_GET['transaksi_tanggal']) && isset($_GET['kprk']) && isset($_GET['regional'])){
+      $tgl_trx = $_GET['transaksi_tanggal'];
+      $kprk       = $_GET['kprk'];
+      $regional   = $_GET['regional'];
+        ?>
 
- 		<div class="row">
- 			<div class="col-lg-6">
- 				<table class="table table-bordered">
- 					<tr>
- 						<th width="30%">DARI TANGGAL</th>
- 						<th width="1%">:</th>
-                      	<td><?php echo $tgl_trx; ?></td>
- 					</tr>
- 					<tr>
- 						<th>DESKRIPSI</th>
- 						<th>:</th>
- 						<th>PENGAWASAN MANIFEST</th>
- 					</tr>
- 				</table>
+  <div class="row">
+  <div class="col-lg-6">
+  <table class="table table-bordered">
+  <tr>
+  <th width="30%">DARI TANGGAL</th>
+  <th width="1%">:</th>
+  <td><?php echo $tgl_trx; ?></td>
+  </tr>
+  <tr>
+  <th>DESKRIPSI</th>
+  <th>:</th>
+  <th>PENGAWASAN MANIFEST</th>
+  </tr>
+  </table>
 
- 			</div>
- 		</div>
+  </div>
+  </div>
 
- 		<div class="table-responsive">
- 			<table class="table table-bordered table-striped">
- 				<thead>
- 					<tr>
-          	<th width="1%" rowspan="2">NO</th>
-            <th>User ID</th>
-            <th>No.KPRK</th>
-            <th>ID PRODUK</th>
-            <th>Jml Trx</th>
-            <th>Jlm Uang</th>
-            <th>Tanggal</th>
-            <th>Nomor Backsheeet</th>
-            <th>Barcode</th>
-            <th>No. Resi</th>
-         </tr>
- 				</thead>
- 				<tbody>
+  <div class="table-responsive">
+  <table class="table table-bordered table-striped">
+  <thead>
+  <tr>
+            <th width="1%" rowspan="2">NO</th>
+             <th>User ID</th>
+             <th>No.KPRK</th>
+             <th>ID PRODUK</th>
+             <th>Jml Trx</th>
+             <th>Jlm Uang</th>
+             <th>Tanggal</th>
+             <th>Nomor Backsheeet</th>
+             <th>Barcode</th>
+             <th>No. Resi</th>
+             </tr>
+  </thead>
+  <tbody>
  					<?php 
             include '../koneksi.php';
             $no=1;
             $query = "SELECT a.*, b.* FROM transaksi as a,t_pegawailoket as b where a.pegawai_id=b.pegawai_id  and date(transaksi_tanggal) ='$tgl_trx'";
 
-            if ($kprk != '00' && $kprk!='') {                                 
-              $query .= "and b.KPRK = '$kprk'";
-            };
+         if ($kprk != '00' && $kprk!='') {                                 
+         $query .= "and b.KPRK = '$kprk'";
+         };
             
             if ($regional !='') {                                 
               $query .= "and a.ref_regional  = '$regional'";
             };
-            $data = mysqli_query($koneksi,$query);
+         $data = mysqli_query($koneksi, $query);
             
             while($d = mysqli_fetch_array($data)){
 
-              if($d['nominal_tagihan']){
+            if($d['nominal_tagihan']){
               $total_uang  = $d['nominal_tagihan'];
-              }
-              ?>
-              <tr>
-                <?php
+            }
+            ?>
+            <tr>
+            <?php
                 if($d['jmlTrx']){
                   $total_uangs  = $d['jmlTrx'];
                 }
